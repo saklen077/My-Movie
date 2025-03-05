6cdb2f24f8fc3c07b929a315f55a6b47f389cc92
saklen077 <110762373+saklen077@users.noreply.github.com>
2025-03-05 23:48:18 +0530

    added avatar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import dp from "../assets/dp.jpg";
 export default function Navbar() {
   return (
     <>
-      <div className="navbar  py-0  text-white">
+      <div className="navbar flex justify-between py-0  text-white">
         <div className="navbar-start mx-[1rem] sm:mx-[2.6rem]">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost md:hidden">
@@ -27,31 +27,80 @@ export default function Navbar() {
               className="menu menu-sm dropdown-content bg-base-100 text-black rounded-box z-[1] mt-3 w-52 p-2 shadow"
             >
               <li>
-                <a href="/movies/top_rated" className="text-[16px] hover:text-red-500"><span>Top rated</span></a>
+                <a
+                  href="/movies/top_rated"
+                  className="text-[16px] hover:text-red-500"
+                >
+                  <span>Top rated</span>
+                </a>
               </li>
               <li>
-                <a href="/movies/popular" className="text-[16px] hover:text-red-500">Popular</a>
+                <a
+                  href="/movies/popular"
+                  className="text-[16px] hover:text-red-500"
+                >
+                  Popular
+                </a>
               </li>
               <li>
-                <a href="/movies/upcoming" className="text-[16px] hover:text-red-500">Upcoming</a>
+                <a
+                  href="/movies/upcoming"
+                  className="text-[16px] hover:text-red-500"
+                >
+                  Upcoming
+                </a>
               </li>
             </ul>
           </div>
-          <a href="/" className=" text-3xl mx-2 md:mr-4 text-red-800 cursor-pointer">MOVIES</a>
+          <a
+            href="/"
+            className=" text-3xl mx-2 md:mr-4 text-red-800 cursor-pointer"
+          >
+            MOVIES
+          </a>
           <div className="navbar-center  hidden md:flex ">
             <ul className="menu menu-horizontal px-2 m-2 ">
               <li>
-                <a href="/movies/top_rated" className="text-[16px] hover:text-red-500"><span>Top rated</span></a>
+                <a
+                  href="/movies/top_rated"
+                  className="text-[16px] hover:text-red-500"
+                >
+                  <span>Top rated</span>
+                </a>
               </li>
               <li>
-                <a href="/movies/popular" className="text-[16px] hover:text-red-500">Popular</a>
+                <a
+                  href="/movies/popular"
+                  className="text-[16px] hover:text-red-500"
+                >
+                  Popular
+                </a>
               </li>
               <li>
-                <a href="/movies/upcoming" className="text-[16px] hover:text-red-500">Upcoming</a>
+                <a
+                  href="/movies/upcoming"
+                  className="text-[16px] hover:text-red-500"
+                >
+                  Upcoming
+                </a>
               </li>
             </ul>
           </div>
-
+        </div>
+        {/* <img src={dp} width={40} height={40}className="mx-2 md:mr-4 border rounded-full"/> */}
+        <div className="dropdown dropdown-end z-1000 mx-2 md:mr-4 mt-2">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar"
+          >
+            <div className="w-15 text-center  rounded-full">
+              <img
+                alt="User Dp"
+                src="https://photosnow.net/wp-content/uploads/2023/12/no-dp48.jpg"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </>
